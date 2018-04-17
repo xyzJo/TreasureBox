f85279f621d91918fa98b6c81e6bffa022e942d1
joanaibarra <joana97.ji@gmail.com>
2018-04-17 13:56:27 -0400

    Initial commit

---
+++ app/src/main/java/com/osproj/joanaibarra/treasurebox/Homescreen.java
@@ -0,0 +1,14 @@
+package com.osproj.joanaibarra.treasurebox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Homescreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+    }
+}
+
