004f122a2fe81c93e11156f7c8a3625291906b35
joanaibarra <joana97.ji@gmail.com>
2018-04-17 22:45:36 -0400

    Add button functionality for existing user

--- app/src/main/java/com/osproj/joanaibarra/treasurebox/homescreen.java
+++ app/src/main/java/com/osproj/joanaibarra/treasurebox/homescreen.java
@@ -1,7 +1,10 @@
 package com.osproj.joanaibarra.treasurebox;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class homescreen extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
+
+        ImageButton enterPin = findViewById(R.id.imageButton_existingUser);
+        enterPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(homescreen.this, FoldersView.class));
+            }
+        });
     }
 }
 
